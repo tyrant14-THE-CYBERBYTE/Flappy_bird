64ae7b375ba115965949f0513a399b610fb14fbf
Baran <57062330+Xenon67@users.noreply.github.com>
2021-02-28 03:52:15 +0300

    Update spammer.js

--- plugins/spammer.js
+++ plugins/spammer.js
@@ -3,6 +3,9 @@ Telegram: t.me/phaticusthiccy
 Instagram: www.instagram.com/kyrie.baran
 */
 
+const fs = require('fs');
+const {MessageType,Mimetype} = require('@adiwajshing/baileys');
+const ffmpeg = require('fluent-ffmpeg');
 const Asena = require('../events');
 const Language = require('../language');
 const Lang = Language.getString('spammer');
@@ -22,8 +25,61 @@ Asena.addCommand({ pattern: 'spam ?(.*)', fromMe: true, desc: Lang.SPAM_DESC }, 
             await message.sendMessage(match[1])
         }
     }
+    else if (message.reply_message.video === false && message.reply_message.image) return await message.sendMessage('```Need Media!```');
 
+    var location = await message.client.downloadAndSaveMediaMessage({
+        key: {
+            remoteJid: message.reply_message.jid,
+            id: message.reply_message.id
+        },
+        message: message.reply_message.data.quotedMessage
+    });
+    ffmpeg(location)
+        .save('output.jpg')
+        .on('end', async () => {
+            if (totalMaxSpamCount !== 0) {
+                for (let index = 0; index < totalMaxSpamCount; index++) {
+                await message.client.sendMessage(message.jid,fs.readFileSync('output.jpg'), MessageType.image, {mimetype: Mimetype.jpg})
+            }
+        }
+    }
+    else if (message.reply_message.image === false && message.reply_message.video) return await message.sendMessage('```Need Media!```');
 
+    var location = await message.client.downloadAndSaveMediaMessage({
+        key: {
+            remoteJid: message.reply_message.jid,
+            id: message.reply_message.id
+        },
+        message: message.reply_message.data.quotedMessage
+    });
+    ffmpeg(location)
+        .save('output.mp4')
+        .on('end', async () => {
+            if (totalMaxSpamCount !== 0) {
+                for (let index = 0; index < totalMaxSpamCount; index++) {
+                await message.client.sendMessage(message.jid,fs.readFileSync('output.mp4'), MessageType.video, {mimetype: Mimetype.mpeg})
+            }
+        }
+    }
+    else {
+
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
+        ffmpeg(location)
+            .save('output.mp3')
+            .on('end', async () => {
+                if (totalMaxSpamCount !== 0) {
+                    for (let index = 0; index < totalMaxSpamCount; index++) {
+                    await message.client.sendMessage(message.jid,fs.readFileSync('output.mp3'), MessageType.audio, {mimetype: Mimetype.mp4Audio})
+                }
+            }
+        }
+    }
 }));
 
 Asena.addCommand({ pattern: 'killspam', fromMe: true, desc: Lang.STOP_SPAMDESC }, (async (message, match) => {
