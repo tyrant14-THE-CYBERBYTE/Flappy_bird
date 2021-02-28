1dd21a5505f5ec8c895b2e4d5071f6f9b28cfdef
Baran <57062330+Xenon67@users.noreply.github.com>
2021-02-28 17:48:28 +0300

    Update someonesay.js

--- plugins/someonesay.js
+++ plugins/someonesay.js
@@ -18,7 +18,7 @@ Asena.addCommand({ pattern: 'animesay ?(.*)', fromMe: true, desc: Lang.ANIME_DES
 
     var ttinullimage = await axios.get(`https://nekobot.xyz/api/imagegen?type=kannagen&text=${match[1].replace(/Ö/g, "%C3%96").replace(/ö/g, "%C3%B6").replace(/ü/g, "%C3%BC").replace(/Ü/g, "%C3%9C").replace(/Ğ/g, "%C4%9E").replace(/ğ/g, "%C4%9F").replace(/ş/g, "%C5%9F").replace(/Ş/g, "%C5%9E").replace(/ç/g, "%C3%A7").replace(/Ç/g, "%C3%87").replace(/ı/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/İ/g, "%C4%B0")}&raw=1`, { responseType: 'arraybuffer' })
 
-    var location = await message.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.png})
+    var location = await message.client.downloadAndSaveMediaMessage({Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.png}})
 
     ffmpeg(location)
         .save('output.webp')
