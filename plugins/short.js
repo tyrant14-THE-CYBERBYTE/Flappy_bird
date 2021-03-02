2169f0166f6449a52c959aa9a4e2b924dad1d879
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-02 20:37:35 +0300

    Create short.js

---
+++ plugins/short.js
@@ -0,0 +1,10 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Asena.addCommand({pattern: 'url', fromMe: true}, (async (message, match) => {
+    if (match[1] == '') return await message.client.sendMessage(message.jid, 'URL Gerekli!', MessageType.text);
+
+    shortenUrl(`${match[1]}`).then((result) => {
+        await message.client.sendMessage(message.jid, '```Uzun Link:``` ' + `${match[1]}` + '\n' + '```Kısa Link:``` ' + { result }); // https://tinyurl.com/<slug>
+    });
+}));
