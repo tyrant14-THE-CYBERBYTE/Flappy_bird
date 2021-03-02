e07b580c8fae7b9499760063c8c49607730675c4
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-02 21:01:14 +0300

    Update short.js

--- plugins/short.js
+++ plugins/short.js
@@ -5,6 +5,6 @@ Asena.addCommand({pattern: 'url', fromMe: true}, (async (message, match) => {
     if (match[1] == '') return await message.client.sendMessage(message.jid, 'URL Gerekli!', MessageType.text);
 
     shortenUrl(`${match[1]}`).then((result) => {
-        await message.client.sendMessage(message.jid, `*Uzun Link:* ${match[1]} \n*Kısa Link:*` + result); // https://tinyurl.com/<slug>
+        await message.client.sendMessage(message.jid,'Kısa Link:*' + result); // https://tinyurl.com/<slug>
     });
 }));
