3e2438dbffdf444f9621d7d1bf35486e6c2cd144
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-08 15:34:26 +0300

    Update ara.js

--- plugins/ara.js
+++ plugins/ara.js
@@ -10,9 +10,9 @@ Asena.addCommand({pattern: 'ara ?(.*)', fromMe: true}, (async (message, match) =
     if (match[1] === '') return await message.client.sendMessage(message.jid, 'Kelime Gir', MessageType.text);
     
     
-    var options = { query: `${match[1]}`,limit: 10 }
+    var options = { query: `${match[1]}`, limit: 10 }
  
-    scraper.search(options, async(err, url, meta) {
+    scraper.search(options, async (err, url, meta) {
         // This is called for each result
         if(err) throw err;
         await message.client.sendMessage(message.jid, url + '\n\n' + meta.title + '\n\n' + meta.meta, MessageType.text);
