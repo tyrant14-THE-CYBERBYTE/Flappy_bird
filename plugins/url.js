106cc9096deddbb54a42432c49308ea50a1adf12
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-08 17:28:35 +0300

    Create url.js

---
+++ plugins/url.js
@@ -0,0 +1,17 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const TinyURL = require('tinyurl');
+ 
+Asena.addCommand({pattern: 'short ?(.*)', fromMe: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,'Link Gir!', MessageType.text);
+
+    TinyURL.shorten(`${match[1]}`, async(res, err) {
+      if (err)
+        await message.client.sendMessage(message.jid, err, MessageType.text)
+
+        await message.client.sendMessage(message.jid, res, MessageType.text)
+
+    });
+}));
