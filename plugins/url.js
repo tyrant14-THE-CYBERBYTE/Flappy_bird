c3ca58d60182e036d121cc69fcffe25e726ca4a9
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-08 19:40:10 +0300

    Update url.js

--- plugins/url.js
+++ plugins/url.js
@@ -1,7 +1,7 @@
 const Asena = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
-const scraper = require('google-search-scraper');
+const Scraper = require('google-search-scraper');
 const TinyURL = require('tinyurl');
 
 const De = "Uzun linkleri kısaltır."
@@ -22,16 +22,12 @@ Asena.addCommand({pattern: 'short ?(.*)', fromMe: true, desc: De}, (async (messa
 Asena.addCommand({pattern: 'search ?(.*)', fromMe: true, desc: Sr}, (async (message, match) => {
 
     if (match[1] === '') return await message.client.sendMessage(message.jid,'```Aranacak Kelime Girmelisin!```', MessageType.text);
-
-
-    var options = {
-      query: `${match[1]}`,
-      limit: 10
-    };
  
-    scraper.search(options, async(err, url, meta) => {
-      // This is called for each result
-      if(err) throw err;
-      await message.client.sendMessage(message.jid, `*${match[1]} İçin Arama Sonuçları:*\n\n` + url, MessageType.text);
+    Scraper.search(query: `${match[1]}`, limit: 8, async(err, url, meta) => {
+
+      if(err)
+        await message.client.sendMessage(message.jid, '*#### Hata! ####*\n' + err, MessageType.text);
+
+        await message.client.sendMessage(message.jid, `*${match[1]} İçin Arama Sonuçları:*\n\n` + url, MessageType.text);
     });
 }));
