33d02ef676c5a5523747e4a95cc54d0769f67c6e
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-08 18:00:41 +0300

    Update url.js

--- plugins/url.js
+++ plugins/url.js
@@ -7,7 +7,7 @@ Asena.addCommand({pattern: 'short ?(.*)', fromMe: true}, (async (message, match)
 
     if (match[1] === '') return await message.client.sendMessage(message.jid,'Link Gir!', MessageType.text);
 
-    TinyURL.shorten(`${match[1]}`).then(async(res) {
+    TinyURL.shorten(`match[1]`).then(async(res) {
         await message.client.sendMessage(message.jid, res, MessageType.text);
     }, async(err) {
         await message.client.sendMessage(message.jid, err, MessageType.text);
