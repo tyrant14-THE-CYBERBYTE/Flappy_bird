7447022f81453945584b734d9c465d028a67d72e
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-08 18:53:49 +0300

    Update url.js

--- plugins/url.js
+++ plugins/url.js
@@ -2,15 +2,16 @@ const Asena = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 const TinyURL = require('tinyurl');
- 
-Asena.addCommand({pattern: 'short ?(.*)', fromMe: true}, (async (message, match) => {
+const De = "Uzun linkleri kısaltır."
 
-    if (match[1] === '') return await message.client.sendMessage(message.jid,'Link Gir!', MessageType.text);
+Asena.addCommand({pattern: 'short ?(.*)', fromMe: true, desc: De}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,'```Link Girmelisin!```', MessageType.text);
 
     TinyURL.shorten(`${match[1]}`, async(res, err) => {
       if (err)
         await message.client.sendMessage(message.jid, '*#### Hata! ####*\n' + err, MessageType.text);
 
-        await message.client.sendMessage(message.jid, `*Kısa Link:* ${match[1]}\n*Kısa Link:* ` + res, MessageType.text)
+        await message.client.sendMessage(message.jid, `*Orjinal Link:* ${match[1]}\n*Kısa Link:* ` + res, MessageType.text)
     });
 }));
