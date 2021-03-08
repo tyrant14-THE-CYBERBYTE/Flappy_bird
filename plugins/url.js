903a7940e0b5ae69e2312956047fd212b2ef7a37
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-08 18:16:15 +0300

    Update url.js

--- plugins/url.js
+++ plugins/url.js
@@ -1,15 +1,13 @@
 const Asena = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
-const TinyURL = require('tinyurl');
- 
+const adfly = require("adf.ly")("24069803","9321766b6eeb1d9fa239b8c755cf7c57"); // You will find your API key in adf.ly Tools section.
+
 Asena.addCommand({pattern: 'short ?(.*)', fromMe: true}, (async (message, match) => {
 
     if (match[1] === '') return await message.client.sendMessage(message.jid,'Link Gir!', MessageType.text);
 
-    TinyURL.shorten(`match[1]`).then(async(res) {
-        await message.client.sendMessage(message.jid, res, MessageType.text);
-    }, async(err) {
-        await message.client.sendMessage(message.jid, err, MessageType.text);
+    adfly.short(`${match[1]}`, async(url){
+        await message.client.sendMessage(message.jid, '*Kısa Link:*' + url, MessageType.text);
     });
 }));
