bff3487e5a71d7a6aa43666055b8b5793300be07
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-12 22:25:47 +0300

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -7,19 +7,36 @@ Asena.addCommand({ pattern: 'igvideo ?(.*)', fromMe: true, desc: sd }, async (me
 
     const userName = match[1]
 
-    if (!userName) return await message.client.sendMessage(message.jid, '```URL Gir!```');
+    if (!userName) return await message.client.sendMessage(message.jid, '```URL Gir!```')
 
-    await axios.get(`https://videfikri.com/api/igdl/?url=${userName}`).then(async (response) => {
+    await axios
+      .get(`https://videfikri.com/api/igdl/?url=${userName}`)
+      .then(async (response) => {
 
-        const {like, comment, full_name, video, duration } = response.data.result
+        const {
+          like, 
+          comment, 
+          full_name, 
+          video, 
+          duration,
+        } = response.data.result
 
-        const igdat = await axios.get(video, {responseType: 'arraybuffer'})
+        const igdat = await axios.get(video, 
+          {responseType: 'arraybuffer',
+        })
 
         const msg = `
         *İsmi:* ${full_name},
         *Beğeni:* ${like},
         *Yorum:* ${comment},
         *Uzunluk:* ${duration}`
-    })
-    await message.sendMessage(Buffer.from(igdat.data), MessageType.video, { caption: msg });
-}));
+
+        await message.sendMessage(Buffer.from(igdat.data), MessageType.video, { 
+          caption: msg,
+        })
+      })
+      .catch(
+        async (err) => await message.client.sendMessage(message.jid, 'Bulunamadı'),
+      )
+  },
+)
