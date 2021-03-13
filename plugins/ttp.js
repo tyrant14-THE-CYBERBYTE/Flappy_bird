96dfbf9f011f48f0ac5043778b245b9f8adde60c
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-14 01:50:15 +0300

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -7,6 +7,7 @@ const Asena = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const axios = require('axios');
+const GW = "Yazıyı dark neon fotoğrafına çevirir."
 
 const Language = require('../language');
 const Lang = Language.getString('ttp');
@@ -100,6 +101,16 @@ Asena.addCommand({ pattern: 'metalttp ?(.*)', fromMe: true, desc: Lang.METAL_DES
     if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
 
     var ttinullimage = await axios.get(`https://api.xteam.xyz/photooxy/crispchrome?text=${match[1].replace(/Ö/g, "%C3%96").replace(/ö/g, "%C3%B6").replace(/ü/g, "%C3%BC").replace(/Ü/g, "%C3%9C").replace(/Ğ/g, "%C4%9E").replace(/ğ/g, "%C4%9F").replace(/ş/g, "%C5%9F").replace(/Ş/g, "%C5%9E").replace(/ç/g, "%C3%A7").replace(/Ç/g, "%C3%87").replace(/ı/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/İ/g, "%C4%B0")}&APIKEY=e67bd1bafe81b611`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made for Founder' })
+
+}));
+
+Asena.addCommand({ pattern: 'glowttp ?(.*)', fromMe: true, desc: GW }, (async (message, match) => {
+
+    if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
+
+    var ttinullimage = await axios.get(`https://videfikri.com/api/textmaker/glowingneon/?text=${match[1].replace(/Ö/g, "%C3%96").replace(/ö/g, "%C3%B6").replace(/ü/g, "%C3%BC").replace(/Ü/g, "%C3%9C").replace(/Ğ/g, "%C4%9E").replace(/ğ/g, "%C4%9F").replace(/ş/g, "%C5%9F").replace(/Ş/g, "%C5%9E").replace(/ç/g, "%C3%A7").replace(/Ç/g, "%C3%87").replace(/ı/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/İ/g, "%C4%B0")}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made for Founder' })
 
