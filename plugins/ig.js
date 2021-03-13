65ce779039a89385802595b87061e07ad1855b60
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-13 12:24:49 +0300

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -2,12 +2,16 @@ const Asena = require('../events')
 const { MessageType, Mimetype} = require('@adiwajshing/baileys')
 const axios = require('axios')
 const sd = "Instagramdan video indirir."
+const gt = "Github profilini gösterir."
+const hb = "Kiralanamaz"
+const yb = "Kiralanabilir"
+
 
 Asena.addCommand({ pattern: 'igvideo ?(.*)', fromMe: true, desc: sd }, async (message, match) => {
 
     const userName = match[1]
 
-    if (!userName) return await message.client.sendMessage(message.jid, '```URL Gir!```')
+    if (userName === '') return await message.client.sendMessage(message.jid, '```URL Gir!```')
 
     await axios
       .get(`https://videfikri.com/api/igdl/?url=${userName}`)
@@ -25,13 +29,54 @@ Asena.addCommand({ pattern: 'igvideo ?(.*)', fromMe: true, desc: sd }, async (me
           {responseType: 'arraybuffer',
         })
 
-        const msg = `
-        *İsmi:* ${full_name},
-        *Beğeni:* ${like},
-        *Yorum:* ${comment},
-        *Uzunluk:* ${duration}`
+        const msg = `*İsmi:* ${full_name} \n*Beğeni:* ${like} \n*Yorum:* ${comment} \n*Uzunluk:* ${duration}`
 
         await message.sendMessage(Buffer.from(igdat.data), MessageType.video, { 
+          caption: msg,
+        })
+      })
+      .catch(
+        async (err) => await message.client.sendMessage(message.jid, 'Bulunamadı'),
+      )
+  },
+)
+
+Asena.addCommand({ pattern: 'github ?(.*)', fromMe: true, desc: gt }, async (message, match) => {
+
+    const userName = match[1]
+
+    if (userName === '') return await message.client.sendMessage(message.jid, '```Github Kullanıcı İsmi Gir!```')
+
+    await axios
+      .get(`https://videfikri.com/api/github/?username=${userName}`)
+      .then(async (response) => {
+
+        const {
+          hireable,
+          company,
+          profile_pic,
+          username,
+          full_name, 
+          blog, 
+          location,
+          email,
+          public_repository,
+          biografi,
+          following,
+          followers,
+          public_gists,
+          profile_url,
+          last_updated,
+          joined_on,
+        } = response.data.result
+
+        const gidat = await axios.get(profile_pic, 
+          {responseType: 'arraybuffer',
+        })
+
+        const msg = `*Kullanıcı Adı:* ${username} \n*İsmi:* ${full_name} \n*Takipçi:* ${followers} \n*Takip Edilen:* ${following} \n*Biyografi:* ${biography} \n*Açık Repolar:* ${public_repository} \n*Açık Gistler:* ${public_gists} \n*Konumu:* ${location} \n*Email:* ${email} \n*Blog:* ${blog} \n*Şirket:* ${company} \n*Kiralanabilir mi?:* ${hireable ? hb : yb} \n*Katılma Tarihi:* ${joined_on} \n*Son Güncelleme Tarihi:* ${last_updated}`
+
+        await message.sendMessage(Buffer.from(gidat.data), MessageType.image, { 
           caption: msg,
         })
       })
