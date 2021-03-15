f898a7948b3b0f8717ff334d94993b9e6473d85d
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-15 14:21:32 +0300

    Update g.js

--- plugins/g.js
+++ plugins/g.js
@@ -1,15 +1,16 @@
 const Asena = require('../events');
 const { MessageType, Mimetype} = require('@adiwajshing/baileys');
 
-const bok = "Ğ gönderir."
+const TRd = "Ğ gönderir."
+const resp = "────────────────\n─██████████████─\n─██░░░░░░░░░░██─\n─██░░██████████─\n─██░░██─────────\n─██░░██─────────\n─██░░██──██████─\n─██░░██──██░░██─\n─██░░██──██░░██─\n─██░░██████░░██─\n─██░░░░░░░░░░██─\n─██████████████─\n────────────────"
 
+Asena.addCommand({ pattern: 'ğ ?(.*)', fromMe: true, desc: TRd }, async (message, match) => {
 
-Asena.addCommand({ pattern: 'g ?(.*)', fromMe: true, desc: bok }, async (message, match) => {
+    if (match[1] === '') return await message.client.sendMessage(message.jid, '────────────────\n─██████████████─\n─██░░░░░░░░░░██─\n─██░░██████████─\n─██░░██─────────\n─██░░██─────────\n─██░░██──██████─\n─██░░██──██░░██─\n─██░░██──██░░██─\n─██░░██████░░██─\n─██░░░░░░░░░░██─\n─██████████████─\n────────────────', MessageType.text);
 
-    if (match[1] === '') {
-        await message.client.sendMessage(message.jid, '────────────────\n─██████████████─\n─██░░░░░░░░░░██─\n─██░░██████████─\n─██░░██─────────\n─██░░██─────────\n─██░░██──██████─\n─██░░██──██░░██─\n─██░░██──██░░██─\n─██░░██████░░██─\n─██░░░░░░░░░░██─\n─██████████████─\n────────────────', MessageType.text);
-    }
     else {
-        await message.client.sendMessage(message.jid, '────────────────\n─██████████████─\n─██░░░░░░░░░░██─\n─██░░██████████─\n─██░░██─────────\n─██░░██─────────\n─██░░██──██████─\n─██░░██──██░░██─\n─██░░██──██░░██─\n─██░░██████░░██─\n─██░░░░░░░░░░██─\n─██████████████─\n────────────────'.replace(/─/g, "${match[1]}"), MessageType.text)
+
+        await message.client.sendMessage(message.jid, resp.replace(/─/g, "${match[1]}").replace(/█/g, "${match[1]}").replace(/░/g, "${match[1]}"), MessageType.text)
+    
     }
 }));
