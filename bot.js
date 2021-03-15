364e5f787c752220691813e11bfeea05548ec845
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-15 19:58:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -159,14 +159,9 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
         }
     });
 
-    conn.on('group-update', async () => {
-        console.log('🆕 Chat Updated!')
-    });
-
     conn.on('message-new', async msg => {
 
-        if (chat.jid == '905524317852-1612300121@g.us' sendMsg = false)
-
+        
         if (msg.key && msg.key.remoteJid == 'status@broadcast') return;
 
         if (config.NO_ONLINE) {
