de23838cfe3b0755a018d6f497430f31004cf34d
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-15 23:59:41 +0300

    Update stickers.js

--- plugins/stickers.js
+++ plugins/stickers.js
@@ -13,7 +13,6 @@ const ffmpeg = require('fluent-ffmpeg');
 const {execFile} = require('child_process');
 const cwebp = require('cwebp-bin');
 
-const TRM = "Animasyonlu stickeri videoya çevirir."
 const Language = require('../language');
 const Lang = Language.getString('sticker');
 
@@ -46,29 +45,5 @@ Asena.addCommand({pattern: 'sticker', fromMe: true, desc: Lang.STICKER_DESC}, (a
         .on('end', async () => {
             await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
         });
-    return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
-}));
-
-Asena.addCommand({pattern: 'mp4roll', fromMe: true, desc: TRM}, (async (message, match) => {    
-
-    if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
-    var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
-    var location = await message.client.downloadAndSaveMediaMessage({
-        key: {
-            remoteJid: message.reply_message.jid,
-            id: message.reply_message.id
-        },
-        message: message.reply_message.data.quotedMessage
-    });
-
-    let eff = "amplify=radius=7";
-
-    execFile('ffmpeg', ['-i', location, '-filter:v', eff, 'output.mp4'], async err => {
-        if (err) {
-            throw err;
-        }
-        
-        await message.sendMessage(fs.readFileSync('./output.mp4'), MessageType.video, {mimetype: Mimetype.mpeg, caption: 'Made for Founder'});
-    });
     return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
 }));
