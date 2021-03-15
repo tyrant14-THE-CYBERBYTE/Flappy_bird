7d5c8af6b63ee364e1be54b39617f3cc39fb80d4
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-15 13:33:26 +0300

    Create Ğ.js

---
+++ plugins/Ğ.js
@@ -0,0 +1,16 @@
+const Asena = require('../events')
+const { MessageType, Mimetype} = require('@adiwajshing/baileys')
+const desc = "Ğ gönderir."
+
+
+Asena.addCommand({ pattern: 'ğ ?(.*)', fromMe: true, desc: desc }, async (message, match) => {
+
+    const gsay = match[1]
+
+    if (gsay === '') {
+        await message.client.sendMessage(message.jid, '────────────────\n─██████████████─\n─██░░░░░░░░░░██─\n─██░░██████████─\n─██░░██─────────\n─██░░██─────────\n─██░░██──██████─\n─██░░██──██░░██─\n─██░░██──██░░██─\n─██░░██████░░██─\n─██░░░░░░░░░░██─\n─██████████████─\n────────────────', MessageType.text);
+    else {
+        await message.client.sendMessage(message.jid, 'Ğ', MessageType.text);
+    }
+})); 
+      
