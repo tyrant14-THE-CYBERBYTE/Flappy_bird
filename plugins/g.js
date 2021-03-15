e7a474ba59eef5851c86b8631954cacd701d7439
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-16 00:25:23 +0300

    Update g.js

--- plugins/g.js
+++ plugins/g.js
@@ -2,8 +2,9 @@ const Asena = require('../events');
 const { MessageType, Mimetype} = require('@adiwajshing/baileys');
 
 const TRd = "Ğ gönderir."
+const TRf = "F gönderir."
 
-Asena.addCommand({ pattern: 'ğ ?(.*)', fromMe: true, desc: TRd }, async (message, match) => {
+Asena.addCommand({ pattern: 'ğ$ ?(.*)', fromMe: true, desc: TRd }, async (message, match) => {
 
     const gsay = match[1]
 
@@ -14,5 +15,19 @@ Asena.addCommand({ pattern: 'ğ ?(.*)', fromMe: true, desc: TRd }, async (messag
     var res = str.replace(/ğ/g, `${gsay}`);
         
     return await message.client.sendMessage(message.jid, res, MessageType.text);
+
+});
+
+Asena.addCommand({ pattern: 'f$ ?(.*)', fromMe: true, desc: TRd }, async (message, match) => {
+
+    const fsay = match[1]
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid, 'ffffffff\nffffffff\nff\nff\nff\nffffff\nffffff\nff\nff\nff\nff\nff', MessageType.text);
+
+    var str = "ffffffff\nffffffff\nff\nff\nff\nffffff\nffffff\nff\nff\nff\nff\nff";
+  
+    var fres = str.replace(/f/g, `${fsay}`);
+        
+    return await message.client.sendMessage(message.jid, fres, MessageType.text);
 
 });
