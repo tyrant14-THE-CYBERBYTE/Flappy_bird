c16b4561b1863ade544daa012bc273cd0869f4d7
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-17 21:23:48 +0300

    Update url.js

--- plugins/url.js
+++ plugins/url.js
@@ -1,14 +1,10 @@
 const Asena = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
-const Sea = require('search-engine-client');
+
 const TinyURL = require('tinyurl');
-const {getLyrics, getSong} = require('genius-lyrics-api');
-const key = "nQKziyUfA84InGMTgFf12_LtuCEgwwg1FoBdCz5rJR9-ue5tBfAGOv2LCpnesWgQ"
 
 const De = "Uzun linkleri kısaltır."
-const Sr = "Googlede arama yapar."
-const ly = "Şarkı hakkında bilgi verir."
 
 Asena.addCommand({pattern: 'short ?(.*)', fromMe: true, desc: De}, (async (message, match) => {
 
@@ -22,31 +18,3 @@ Asena.addCommand({pattern: 'short ?(.*)', fromMe: true, desc: De}, (async (messa
     });
 }));
 
-Asena.addCommand({pattern: 'lyric ?(.*)', fromMe: true, desc: ly}, (async (message, match) => {
-
-    if (match[1] === '') return await message.client.sendMessage(message.jid,'```Sarkı İsmi Girmelisin!```', MessageType.text);
-
-    const options = {
-	    apiKey: key,
-	    title: `${match[1]}`,
-            artist: 'WhatsAsena',
-	    optimizeQuery: true
-    };
-
-    getSong(options).then(async(song) => {
-
-        const pp = `${song.albumArt}`
-
-        await message.client.sendMessage(message.jid, pp, MessageType.image, {caption: `*Şarkı ID:* ${song.id} \n*Şarkı Linki:* ${song.url} \n*Sözleri:*\n\n${song.lyrics}`});
-
-    });
-}));
-
-Asena.addCommand({pattern: 'search ?(.*)', fromMe: true, desc: Sr}, (async (message, match) => {
-
-    if (match[1] === '') return await message.client.sendMessage(message.jid,'```Aranacak Kelime Girmelisin!```', MessageType.text);
- 
-    Sea.google(`${match[1]}`).then(async(result) => {
-        await message.client.sendMessage(message.jid,result, MessageType.text);
-    });
-}));
