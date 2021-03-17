e2a97f3e4806ccecdf3a03e8c01400bd5c23eeed
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-17 14:21:37 +0300

    Update spammer.js

--- plugins/spammer.js
+++ plugins/spammer.js
@@ -5,8 +5,17 @@ Instagram: www.instagram.com/kyrie.baran
 
 const Asena = require('../events');
 const {MessageType, Mimetype} = require('@adiwajshing/baileys');
+const Config = require('../config');
+const Heroku = require('heroku-client');
+
 const Language = require('../language');
 const Lang = Language.getString('spammer');
+
+const heroku = new Heroku({
+    token: Config.HEROKU.API_KEY
+});
+
+let baseURI = '/apps/' + Config.HEROKU.APP_NAME;
 
 let totalMaxSpamCount = 5000
 
@@ -28,8 +37,11 @@ Asena.addCommand({ pattern: 'spam ?(.*)', fromMe: true, desc: Lang.SPAM_DESC }, 
 }));
 
 Asena.addCommand({ pattern: 'killspam', fromMe: true, desc: Lang.STOP_SPAMDESC }, (async (message, match) => {
-
-    totalMaxSpamCount = 0
+    
     return await message.client.sendMessage(message.jid,Lang.STOP_SPAM, MessageType.text);
 
+    console.log(baseURI)
+    await heroku.delete(baseURI + '/dynos').catch(async (error) => {
+        await message.client.sendMessage(message.jid,error.message, MessageType.text);
+    });
 }));
