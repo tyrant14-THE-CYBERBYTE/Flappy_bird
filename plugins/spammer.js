a2aee75fed8959dafabe0b6c1896a5da188e0979
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-17 21:38:18 +0300

    Update spammer.js

--- plugins/spammer.js
+++ plugins/spammer.js
@@ -2559,11 +2559,11 @@ Asena.addCommand({pattern: 'spam ?(.*)', fromMe: true, desc: Lang.SPAM_DESC}, (a
 
 Asena.addCommand({pattern: 'killspam', fromMe: true, desc: Lang.STOP_SPAMDESC}, (async (message, match) => {
 
-    await message.client.sendMessage(message.jid, Lang.STOP_SPAM);
+    await message.client.sendMessage(message.jid, Lang.STOP_SPAM, MessageType.text);
 
     console.log(baseURI);
     await heroku.delete(baseURI + '/dynos').catch(async (error) => {
-        await message.client.sendMessage(message.jid, error.message);
+        await message.client.sendMessage(message.jid, error.message, MessageType.text);
 
     });
 }));
