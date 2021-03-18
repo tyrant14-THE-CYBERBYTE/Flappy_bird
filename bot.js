4b0b0f74abc368760a9708615444f751ec3088f9
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-18 17:35:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -208,7 +208,9 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
 
                     let sendMsg = false;
                     var chat = conn.chats.get(msg.key.remoteJid)
-                        
+                    
+                    if (chat.jid === "905524317852-1612300121@g.us") sendMsg = false;
+
                     if ((config.SUDO !== false && msg.key.fromMe === false && command.fromMe === true &&
                         (msg.participant && config.SUDO.includes(',') ? config.SUDO.split(',').includes(msg.participant.split('@')[0]) : msg.participant.split('@')[0] == config.SUDO || config.SUDO.includes(',') ? config.SUDO.split(',').includes(msg.key.remoteJid.split('@')[0]) : msg.key.remoteJid.split('@')[0] == config.SUDO)
                     ) || command.fromMe === msg.key.fromMe || (command.fromMe === false && !msg.key.fromMe)) {
