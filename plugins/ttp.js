34a7212e83577bacb28fe080107e719575a84195
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-18 18:57:33 +0300

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -13,9 +13,14 @@ const NM = "Anime banner gönderir."
 const Language = require('../language');
 const Lang = Language.getString('ttp');
 
-Asena.addCommand({ pattern: 'ttp ?(.*)', fromMe: true, desc: Lang.TTP_DESC }, (async (message, group, match) => {
-                    
-    if (match[1] === '') return await message.client.sendMessage(group.gid, Lang.NEED_WORD, MessageType.text);
+Asena.addCommand({ pattern: 'ttp ?(.*)', fromMe: true, desc: Lang.TTP_DESC }, (async (message, match) => {
+    
+    if (message.jid === '905524317852-1612300121@g.us') {
+
+        return;
+    }
+           
+    if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_WORD, MessageType.text);
 
     var ttinullimage = await axios.get(`https://api.xteam.xyz/ttp?file&text=${match[1].replace(/Ö/g, "%C3%96").replace(/ö/g, "%C3%B6").replace(/ü/g, "%C3%BC").replace(/Ü/g, "%C3%9C").replace(/Ğ/g, "%C4%9E").replace(/ğ/g, "%C4%9F").replace(/ş/g, "%C5%9F").replace(/Ş/g, "%C5%9E").replace(/ç/g, "%C3%A7").replace(/Ç/g, "%C3%87").replace(/ı/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/İ/g, "%C4%B0")}`, { responseType: 'arraybuffer' })
 
