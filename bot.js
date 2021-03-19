239da485a7b61cd3f6f403cf3a371fbe964b5ec0
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-19 12:53:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -176,7 +176,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
                     await conn.sendMessage(conn.user.jid, '*WhatsAsena Public Olarak Çalışıyor! 🐺*\n\n_Lütfen burada plugin denemesi yapmayın. Burası sizin LOG numaranızdır._\n_Herhangi bir sohbette komutları deneyebilirsiniz :)_\n\n*Botunuz herkese açık bir şekilde çalışmaktadır. Değiştirmek için config vars üzerinden “WORK_TYPE” anahtarını “private” yapın.*\n\n*WhatsAsena Kullandığın İçin Teşekkürler 💌*', MessageType.text);
 
                     await git.fetch();
-                    var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
+                    var commits = await git.log([config.BRANCH + '..origin/' + config.BRANCH]);
                     if (commits.total === 0) {
                         await message.client.sendMessage(
                             message.jid,
@@ -219,7 +219,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
                     await conn.sendMessage(conn.user.jid, '*WhatsAsena Working as Public! 🐺*\n\n_Please do not try plugins here. This is your LOG number._\n_You can try commands to any chat :)_\n\n*Your bot working as public. To change it, make the “WORK_TYPE” switch “private” in config vars.*\n\n*Thanks for using WhatsAsena 💌*', MessageType.text);
 
                     await git.fetch();
-                    var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
+                    var commits = await git.log([config.BRANCH + '..origin/' + config.BRANCH]);
                     if (commits.total === 0) {
                         await message.client.sendMessage(
                             message.jid,
@@ -266,7 +266,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
                     await conn.sendMessage(conn.user.jid, '*WhatsAsena Private Olarak Çalışıyor! 🐺*\n\n_Lütfen burada plugin denemesi yapmayın. Burası sizin LOG numaranızdır._\n_Herhangi bir sohbette komutları deneyebilirsiniz :)_\n\n*Botunuz sadece size özel olarak çalışmaktadır. Değiştirmek için config vars üzerinden “WORK_TYPE” anahtarını “public” yapın.*\n\n*WhatsAsena Kullandığın İçin Teşekkürler 💌*', MessageType.text);
 
                     await git.fetch();
-                    var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
+                    var commits = await git.log([config.BRANCH + '..origin/' + config.BRANCH]);
                     if (commits.total === 0) {
                         await message.client.sendMessage(
                             message.jid,
@@ -310,7 +310,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
                     await conn.sendMessage(conn.user.jid, '*WhatsAsena Working as Private! 🐺*\n\n_Please do not try plugins here. This is your LOG number._\n_You can try commands to any chat :)_\n\n*Your bot working as private. To change it, make the “WORK_TYPE” switch “public” in config vars.*\n\n*Thanks for using WhatsAsena 💌*', MessageType.text);
 
                     await git.fetch();
-                    var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
+                    var commits = await git.log([config.BRANCH + '..origin/' + config.BRANCH]);
                     if (commits.total === 0) {
                         await message.client.sendMessage(
                             message.jid,
