2051bc988c7d64f52983d4358aaf5f4606cfaea2
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-20 23:49:24 +0300

    Update Video.js

--- whatsasena/Video.js
+++ whatsasena/Video.js
@@ -18,7 +18,7 @@ class Video extends Base {
     }
 
     _patch(data) {
-        this.id = data.key.id === undefined ? undefined : data.key.id;
+        this.id = data.key.id;
         this.jid = data.key.remoteJid;
         this.fromMe = data.key.fromMe;
         this.caption = data.message.videoMessage.caption === null ? data.message.videoMessage.caption : '';
