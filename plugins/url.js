e1744ca428cbd14514bf4cf74045d287a100176e
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-21 19:36:34 +0300

    Update url.js

--- plugins/url.js
+++ plugins/url.js
@@ -1,6 +1,7 @@
 const Asena = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
+const { requestLyricsFor, requestAuthorFor, requestTitleFor, requestIconFor } = require("solenolyrics");
 
 const TinyURL = require('tinyurl');
 
@@ -18,3 +19,15 @@ Asena.addCommand({pattern: 'short ?(.*)', fromMe: true, desc: De}, (async (messa
     });
 }));
 
+const def = "```Şarkı İsmi Gir!```"
+const del = "Şarkı sözlerini bulur."
+
+Asena.addCommand({pattern: 'lyric ?(.*)', fromMe: true, desc: del}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,def, MessageType.text);
+
+    var aut = await solenolyrics.requestLyricsFor(`${match[1]}`); 
+    
+    await message.client.sendMessage(message.jid, `*İşte ${match[1]} Adlı Şarkı İçin Şarkı Sözleri:*\n\n` + aut, MessageType.text);
+
+}));
