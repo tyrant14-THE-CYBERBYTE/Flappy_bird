f2895418a7a9b2dc25026dd20fb99bb414172e58
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-23 22:11:29 +0300

    Update language.js

--- language.js
+++ language.js
@@ -13,23 +13,25 @@ if (fs.existsSync('./language/' + Config.LANG + '.json')) {
     var json = JSON.parse(fs.readFileSync('./language/' + Config.LANG + '.json'));
 }
 else {
-    if (Config.LANG == 'TR' || Config.LANG == 'AZ') {
-        conn.sendMessage(
-            conn.user.jid,
-            '_Görünüşe Göre Yanlış Bir Dil Ayarladınız._\_Kullanabileceğiniz Diller:_\n\n🔸 *TR - Türkçe* \n🔸 *AZ - Azerice* \n🔸 *en - İngilizce* \n🔸 *ml - Malayam Dili* \n🔸 *HI - Hintçe* \n🔸 *ES - İspanyolca* \n🔸 *ID - Endonezce* \n\n_Varsayılan Olarak Türkçe Ayarlıyorum.._',
-            MessageType.text
-        );
+    function Asena() => {
+        if (Config.LANG == 'TR' || Config.LANG == 'AZ') {
+            conn.sendMessage(
+                conn.user.jid,
+                '_Görünüşe Göre Yanlış Bir Dil Ayarladınız._\_Kullanabileceğiniz Diller:_\n\n🔸 *TR - Türkçe* \n🔸 *AZ - Azerice* \n🔸 *en - İngilizce* \n🔸 *ml - Malayam Dili* \n🔸 *HI - Hintçe* \n🔸 *ES - İspanyolca* \n🔸 *ID - Endonezce* \n\n_Varsayılan Olarak Türkçe Ayarlıyorum.._',
+                MessageType.text
+            );
 
-        var json = JSON.parse(fs.readFileSync('./language/TR.json'));
-    }
-    else {
-        conn.sendMessage(
-            conn.user.jid,
-            '_It Looks Like You Set the Wrong Language._\n_Existing Languages:_\n\n🔸 *TR - Turkish* \n🔸 *AZ - Azerbaijani* \n🔸 *en - English* \n🔸 *ml - Malayam* \n🔸 *HI - Hindi* \n🔸 *ES - Spanish* \n🔸 *ID - Indonesian* \n\n_I Will Set English As Default.._',
-            MessageType.text
-        );
+            var json = JSON.parse(fs.readFileSync('./language/TR.json'));
+        }
+        else {
+            conn.sendMessage(
+                conn.user.jid,
+                '_It Looks Like You Set the Wrong Language._\n_Existing Languages:_\n\n🔸 *TR - Turkish* \n🔸 *AZ - Azerbaijani* \n🔸 *en - English* \n🔸 *ml - Malayam* \n🔸 *HI - Hindi* \n🔸 *ES - Spanish* \n🔸 *ID - Indonesian* \n\n_I Will Set English As Default.._',
+                MessageType.text
+            );
 
-        var json = JSON.parse(fs.readFileSync('./language/EN.json'));
+            var json = JSON.parse(fs.readFileSync('./language/EN.json'));
+        }
     }
 }
 
