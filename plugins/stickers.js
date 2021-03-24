fdfe64c618ddaebdc1b0d9475c9fff15c53170ff
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-24 16:45:48 +0300

    Update stickers.js

--- plugins/stickers.js
+++ plugins/stickers.js
@@ -1,11 +1,3 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
-*/
-
 const Asena = require('../events');
 const {MessageType, Mimetype} = require('@adiwajshing/baileys');
 const fs = require('fs');
@@ -29,68 +21,21 @@ Asena.addCommand({pattern: 'sticker', fromMe: true, desc: Lang.STICKER_DESC}, (a
     });
 
     if (message.reply_message.video === false && message.reply_message.image) {
-        ffmpeg(location)
-            .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-an", "-vsync 0"])
-            .aspect('1:1')
-            .autopad('black')
-            .save('stickerphoto.webp')
-            .on('end', async () => {
-                await message.sendMessage(fs.readFileSync('stickerphoto.webp'), MessageType.sticker);
+        execFile(cwebp, [location, '-o', 'output.webp'], async err => {
+            if (err) {
+                throw err;
+            }
+        
+            await message.sendMessage(fs.readFileSync('./output.webp'), MessageType.sticker);
         });
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }
 
     ffmpeg(location)
-        .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-loop 0", "-an", "-vsync 0"])
-        .aspect('1:1')
-        .autopad('black')
+        .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-loop 0", "-an", "-vsync 0", "-s 512x512"])
         .save('sticker.webp')
         .on('end', async () => {
             await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
         });
     return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
 }));
-const li = "Renklendirilecek YouTube Linki Girin!"
-const lip = "Videoları Renklendirir."
-
-Asena.addCommand({pattern: 'colorvideo', fromMe: true, desc: lip}, (async (message, match) => {    
-
-    if (match[1] == '') return await message.client.sendMessage(message.jid,li , MessageType.text);
-
-    execFile('git clone https://github.com/jantic/DeOldify.git DeOldify')
-
-    execFile('cd DeOldify')
-
-    execFile('import fastai')
-
-    execFile('from deoldify.visualize import *')
-
-    execFile('torch.backends.cudnn.benchmark=True')
-
-    execFile('from pathlib import Path')
-
-    execFile('torch.backends.cudnn.benchmark=True')
-
-    execFile('import warnings')
-
-    execFile('warnings.filterwarnings("ignore", category=UserWarning, message=".*?Your .*? set is empty.*?")')
-
-    execFile("mkdir 'models'")
-
-    execFile('wget https://data.deepai.org/deoldify/ColorizeVideo_gen.pth -O ./models/ColorizeVideo_gen.pth')
-
-    execFile('apt install ffmpeg')
-
-    execFile('colorizer = get_video_colorizer()')
-
-    execFile('source_url = ' + "'" + match[1] + "'" + '#@param {type:"string"}')
-
-    execFile('render_factor = 20  #@param {type: "slider", min: 5, max: 40}')
-
-    execFile('watermarked = False #@param {type:"boolean"}')
-
-    execFile("video_path = colorizer.colorize_from_url(source_url, 'video.mp4', render_factor, watermarked=watermarked)")
-
-    await message.sendMessage(fs.readFileSync('/DeOldify/result/video.mp4'), MessageType.video);
-}));
- 
