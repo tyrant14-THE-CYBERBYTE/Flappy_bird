7e039ec81d31c015498d7a01c22caa701698d850
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-27 00:04:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -521,17 +521,17 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
                                     );
                                 }
                             }
-                        }
-                        else {
-                            await conn.sendMessage(conn.user.jid, '*-- ERROR REPORT [WHATSASENA] --*' + 
-                                '\n*WhatsAsena an error has occurred!*'+
-                                '\n_This error log may include your number or the number of an opponent. Please be careful with it!_' +
-                                '\n_You can write to our Telegram group for help._' +
-                                '\n_Aslo you can join our support group:_ https://chat.whatsapp.com/JjvOISnxu4z6sv4hx3FBNQ' +
-                                '\n_This message should have gone to your number (saved messages)._\n\n' +
-                                '*Error:* ```' + error + '```\n\n'
-                                , MessageType.text, {detectLinks: false}
-                            );                          
+                            else {
+                                await conn.sendMessage(conn.user.jid, '*-- ERROR REPORT [WHATSASENA] --*' + 
+                                    '\n*WhatsAsena an error has occurred!*'+
+                                    '\n_This error log may include your number or the number of an opponent. Please be careful with it!_' +
+                                    '\n_You can write to our Telegram group for help._' +
+                                    '\n_Aslo you can join our support group:_ https://chat.whatsapp.com/JjvOISnxu4z6sv4hx3FBNQ' +
+                                    '\n_This message should have gone to your number (saved messages)._\n\n' +
+                                    '*Error:* ```' + error + '```\n\n'
+                                    , MessageType.text, {detectLinks: false}
+                                );    
+                            }                      
                         }
                     }
                 }
