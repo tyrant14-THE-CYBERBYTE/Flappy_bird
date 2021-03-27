b34bc7294b2e4a7df385b995013d4e2a7eb8f5db
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-27 23:55:57 +0300

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -49,7 +49,7 @@ Asena.addCommand({pattern: 'asena ?(.*)', fromMe: true, dontAddCommandList: true
             async (command) =>  {
                 if (command.dontAddCommandList || command.pattern === undefined) return;
                 try {
-                    var cmatch = command.pattern.toString().match(/(\W*)([A-Za-züşği öç1234567890]*)/);
+                    var cmatch = command.pattern.toString().match(/(\W*)([A-Za-züşğiöç1234567890]*)/);
                 } catch {
                     var cmatch = [command.pattern];
                 }
