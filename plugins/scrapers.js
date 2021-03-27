f8bddef966a13af603fabfe0fe10d12ead19939a
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-27 16:05:01 +0300

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -325,7 +325,7 @@ const sh = "Yanıtlanan ses dosyasının içindeki müziği bulur."
 
 Asena.addCommand({pattern: 'shazam', fromMe: true, desc: sh }, (async (message, match) => { 
 
-    if (message.reply_message.audio === false) return await message.client.sendMessage(message.jid, bix.UV_REPLY, MessageType.text);
+    if (message.reply_message === false) return await message.client.sendMessage(message.jid, bix.UV_REPLY, MessageType.text);
 
     var location = await message.client.downloadAndSaveMediaMessage({
         key: {
@@ -341,7 +341,7 @@ Asena.addCommand({pattern: 'shazam', fromMe: true, desc: sh }, (async (message, 
         .on('end', async () => {
 
             var data = { 'api_token': '6a03ebccfc18bb19e3fb7bb5280a51ab', 'file': fs.createReadStream('/root/WhatsAsenaDuplicated/lyr.mp3'), 'return': 'apple_music,spotify' };
-            req({ uri: 'https://api.audd.io/', form: data, method: 'POST' }, async (err, res, body) => {
+            req ({ uri: 'https://api.audd.io/', form: data, method: "POST" }, async (err, res, body) => {
                 return await message.client.sendMessage(message.jid, body, MessageType.text);
             })
         })
