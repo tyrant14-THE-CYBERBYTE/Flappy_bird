4d16b5a8380d5027f87a79afe75b6a2f2c00b822
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-27 15:01:17 +0300

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -26,6 +26,7 @@ const yts = require( 'yt-search' )
 const got = require("got");
 const ID3Writer = require('browser-id3-writer');
 const SpotifyWebApi = require('spotify-web-api-node');
+const req = require('request');
 
 const spotifyApi = new SpotifyWebApi({
     clientId: 'acc6302297e040aeb6e4ac1fbdfd62c3',
@@ -34,6 +35,7 @@ const spotifyApi = new SpotifyWebApi({
 //=====================================================================================
 const Language = require('../language');
 const Lang = Language.getString('scrapers');
+const bix = Language.getString('unvoice');
 
 const wiki = require('wikijs').default;
 var gis = require('g-i-s');
@@ -319,3 +321,35 @@ else if (config.WORKTYPE == 'public') {
         }
     }));
 }
+const sh = "Yanıtlanan ses dosyasının içindeki müziği bulur."
+
+Asena.addCommand({pattern: 'shazam ?(.*)', fromMe: true, desc: sh }, (async (message, match) => { 
+
+    if (message.reply_message.audio === false) return await message.client.sendMessage(message.jid, bix.UV_REPLY, MessageType.text);
+
+    var location = await message.client.downloadAndSaveMediaMessage({
+        key: {
+            remoteJid: message.reply_message.jid,
+            id: message.reply_message.id
+        },
+        message: message.reply_message.data.quotedMessage
+    });
+    ffmpeg(location)
+        .format('mp3')
+        .save('lyr.mp3')
+        .on('end', async () => {
+
+            var data = {
+                'api_token': '6a03ebccfc18bb19e3fb7bb5280a51ab',
+                'file': 'https://audd.tech/example1.mp3',
+                'return': 'apple_music,spotify',
+            };
+            req({
+                uri: 'https://api.audd.io/,
+                form: data,
+                method: 'POST'
+              }, async (err, res, body) {
+              return await message.client.sendMessage(message.jid, body, MessageType.text);
+            });
+        });
+}));
