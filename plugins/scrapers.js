1a1a0ba4d39a67e706493fb7b22cdaf51909fe07
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-27 15:32:28 +0300

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -323,7 +323,7 @@ else if (config.WORKTYPE == 'public') {
 }
 const sh = "Yanıtlanan ses dosyasının içindeki müziği bulur."
 
-Asena.addCommand({pattern: 'shazam ?(.*)', fromMe: true, desc: sh }, (async (message, match) => { 
+Asena.addCommand({pattern: 'shazam', fromMe: true, desc: sh }, (async (message, match) => { 
 
     if (message.reply_message.audio === false) return await message.client.sendMessage(message.jid, bix.UV_REPLY, MessageType.text);
 
@@ -340,7 +340,7 @@ Asena.addCommand({pattern: 'shazam ?(.*)', fromMe: true, desc: sh }, (async (mes
         .save('lyr.mp3')
         .on('end', async () => {
 
-            var data = { 'api_token': '6a03ebccfc18bb19e3fb7bb5280a51ab', 'file': fs.createReadStream('lyr.mp3'), 'return': 'apple_music,spotify' };
+            var data = { 'api_token': '6a03ebccfc18bb19e3fb7bb5280a51ab', 'file': fs.createReadStream('/root/WhatsAsenaDuplicated/lyr.mp3'), 'return': 'apple_music,spotify' };
             req({ uri: 'https://api.audd.io/', form: data, method: 'POST' }, async (err, res, body) => {
                 return await message.client.sendMessage(message.jid, body, MessageType.text);
             })
