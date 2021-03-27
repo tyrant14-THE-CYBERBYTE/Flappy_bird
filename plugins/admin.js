6f860d4110a95e3fe7a0d5da1ca9b22ab6db2c75
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-28 01:44:24 +0300

    Update admin.js

--- plugins/admin.js
+++ plugins/admin.js
@@ -13,7 +13,7 @@ const Config = require('../config');
 
 const Language = require('../language');
 const Lang = Language.getString('admin');
-const mux = Language.getString('mute');
+const mut = Language.getString('mute');
 
 async function checkImAdmin(message, user = message.client.user.jid) {
     var grup = await message.client.groupMetadata(message.jid);
