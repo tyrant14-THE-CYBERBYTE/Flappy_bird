687b787d85bc6ccba843230fa0cb7a85ff9b1f10
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-28 22:47:07 +0300

    Update g.js

--- plugins/g.js
+++ plugins/g.js
@@ -17,11 +17,15 @@ const ENF = "Send F word."
 const ENX = "Send X word."
 
 const das = "Grup metada verisini çeker."
+
 Asena.addCommand({ pattern: 'whois', fromMe: true, desc: das }, async (message, match) => { 
 
     const ppUrl = await message.client.getProfilePicture(message.jid) 
+
     await message.client.groupMetadata(message.jid) 
-    await message.client.sendMessage(Buffer.from(ppUrl.data), MessageType.image, {caption: "*Group ID:* " + json.id + "\n*Group Name:* " + json.subject + "\n*Description:* " + json.desc});
+
+    await message.client.sendMessage(Buffer.from(ppUrl.data), MessageType.image, { caption: "*Group ID:* " + json.id + "\n*Group Name:* " + json.subject + "\n*Description:* " + json.desc })
+
 }));
 
 
