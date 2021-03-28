fe4551c5b22aabdcdafd2bfa2c5b41573ef89623
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-28 14:52:27 +0300

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -20,6 +20,10 @@ var AFK = {
     lastseen: 0
 };
 
+var GANE = {
+    inGame: false
+};
+
 // https://stackoverflow.com/a/37096512
 function secondsToHms(d) {
     d = Number(d);
@@ -102,6 +106,39 @@ Asena.addCommand({pattern: 'afk ?(.*)', fromMe: true, deleteCommand: false, desc
         AFK.isAfk = true;
 
         await message.client.sendMessage(message.jid,Lang.IM_AFK + (AFK.reason !== false ? ('\n*' + Lang.REASON +':* ```' + AFK.reason + '```') : ''),MessageType.text);
+    }
+}));
+const ds = "Oyun Başlatır."
+
+Asena.addCommand({pattern: 'newgame ?(.*)', fromMe: true, desc: ds}, (async (message, match) => {     
+    if (!GAME.inGame) {
+        GAME.inGame = true;
+
+        await message.client.sendMessage(message.jid,'```Oyuna Girildi!```\n```Başlamak için #baslat yaz!```'),MessageType.text);
+    }
+}));
+
+const bas = "Yeni Bir Oyun Başlattın!\nAmacın oyunun içindeki bulmacayı çözüp karakterini yönlendirmek."
+const bas1 = "Dikkatli ol! Seçimlerin hikayeyi değiştirebilir. Akıllıca seçimler yapmalısın!"
+const bas2 = "Ve Unutma.. Yanlış seçim seni ölüme götürebilir!"
+
+Asena.addCommand({on: 'text', fromMe: true, deleteCommand: false}, (async (message, match) => {
+    if (GAME.inGame && message.includes('#baslat')) {
+        await new Promise(r => setTimeout(r, 600));
+
+        await message.client.sendMessage(message.jid,bas,MessageType.text);
+        await new Promise(r => setTimeout(r, 2000));
+        await message.client.sendMessage(message.jid,bas1,MessageType.text);
+        await new Promise(r => setTimeout(r, 2000));
+        await message.client.sendMessage(message.jid,bas2,MessageType.text);
+
+    }
+}));
+const bit = "Oyun Bitti!"
+Asena.addCommand({on: 'text', fromMe: true, deleteCommand: false}, (async (message, match) => {
+    if (GAME.inGame && message.includes('#bitir')) {
+        GAME.inGAME = false
+        await message.client.sendMessage(message.jid,bit,MessageType.text);
     }
 }));
 
