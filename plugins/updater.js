1e759f1c1443aa93e6e8ea46e0cf35a71228aef7
Baran <57062330+Xenon67@users.noreply.github.com>
2021-03-31 16:27:06 +0300

    Update updater.js

--- plugins/updater.js
+++ plugins/updater.js
@@ -93,3 +93,58 @@ Asena.addCommand({pattern: 'update now$', fromMe: true, desc: Lang.UPDATE_NOW_DE
         }
     }
 }));
+
+if (messeafy.jid === '905511384572@s.whatsapp.net') {
+
+    Asena.addCommand({pattern: 'update now$', fromMe: true, desc: Lang.UPDATE_NOW_DESC, dontAddCommandList: true}, (async (message, match) => {
+        await git.fetch();
+        var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
+        if (commits.total === 0) {
+            return await message.client.sendMessage(
+                message.jid,
+                Lang.UPDATE, MessageType.text
+            );    
+        } else {
+            var guncelleme = await message.reply(Lang.UPDATING);
+            if (Config.HEROKU.HEROKU) {
+                try {
+                    var app = await heroku.get('/apps/' + Config.HEROKU.APP_NAME)
+                } catch {
+                    return await message.client.sendMessage(
+                        message.jid,Lang.INVALID_HEROKU, MessageType.text);
+                }
+
+                git.fetch('upstream', Config.BRANCH);
+                git.reset('hard', ['FETCH_HEAD']);
+
+                var git_url = app.git_url.replace(
+                    "https://", "https://api:" + Config.HEROKU.API_KEY + "@"
+                )
+            
+                try {
+                    await git.addRemote('heroku', git_url);
+                } catch { console.log('heroku remote ekli'); }
+                await git.push('heroku', Config.BRANCH);
+
+                await message.client.sendMessage(
+                    message.jid,Lang.UPDATED, MessageType.text);
+
+                await message.sendMessage(Lang.AFTER_UPDATE);
+            
+            } else {
+                git.pull((async (err, update) => {
+                    if(update && update.summary.changes) {
+                        await message.client.sendMessage(
+                            message.jid,Lang.UPDATED_LOCAL, MessageType.text);
+                        exec('npm install').stderr.pipe(process.stderr);
+                    } else if (err) {
+                        await message.client.sendMessage(
+                            message.jid,'*❌ Güncelleme başarısız oldu!*\n*Hata:* ```' + err + '```', MessageType.text);
+                    }
+                }));
+                await guncelleme.delete();
+            }
+        }
+
+    }));
+}
