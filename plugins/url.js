00d96c7ed5be86e77ff9940e72a7899ad6157778
Baran <57062330+Xenon67@users.noreply.github.com>
2021-04-06 17:31:40 +0300

    Update url.js

--- plugins/url.js
+++ plugins/url.js
@@ -65,90 +65,21 @@ if (cn.WORKTYPE == 'private') {
         const userName = match[1]
 
         if (userName === '') return await message.sendMessage(errorMessage(Lang.NEED_WORD))
-        if (userName.length <= 30) {
 
-            await message.sendMessage(infoMessage(Lang.LOADING))
+        await message.sendMessage(infoMessage(Lang.LOADING))
 
-            await axios.get(`https://docs-jojo.herokuapp.com/api/stalk?username=${userName}`).then(async (response) => {
+        await axios.get(`https://docs-jojo.herokuapp.com/api/stalk?username=${userName}`).then(async (response) => {
 
-                const {biography, username, edge_follow, edge_followed_by, category_name, is_verified, is_private, edge_owner_to_timeline_media, profile_pic_url_hd, full_name, is_business_account } = response.data.result.graphql.user
+            const {biography, username, edge_follow, edge_followed_by, category_name, is_verified, is_private, edge_owner_to_timeline_media, profile_pic_url_hd, full_name, is_business_account } = response.data.graphql.user
 
-                const profileBuffer = await axios.get(profile_pic_url_hd, { responseType: 'arraybuffer' })
+            const profileBuffer = await axios.get(profile_pic_url_hd, { responseType: 'arraybuffer' })
 
-                const msg = `*${Lang.NAME}*: ${full_name} \n*${Lang.USERNAME}*: ${username} \n*${Lang.BIO}*: ${biography} \n*${Lang.FOLLOWERS}*: ${edge_followed_by.count} \n*${Lang.FOLLOWS}*: ${edge_follow.count} \n*${Lang.ACCOUNT}*: ${is_private ? Lang.HIDDEN : Lang.PUBLIC} \n*Hesap Türü:* ${is_verified ? vf : novf} \n*İşletme Hesabı mı?:* ${is_business_account ? bs : nobs} \n*Kategori:* ${category_name} \n*Post Sayısı:* ${edge_owner_to_timeline_media.count}`
+            const msg = `*${Lang.NAME}*: ${full_name} \n*${Lang.USERNAME}*: ${username} \n*${Lang.BIO}*: ${biography} \n*${Lang.FOLLOWERS}*: ${edge_followed_by.count} \n*${Lang.FOLLOWS}*: ${edge_follow.count} \n*${Lang.ACCOUNT}*: ${is_private ? Lang.HIDDEN : Lang.PUBLIC} \n*Hesap Türü:* ${is_verified ? vf : novf} \n*İşletme Hesabı mı?:* ${is_business_account ? bs : nobs} \n*Kategori:* ${category_name} \n*Post Sayısı:* ${edge_owner_to_timeline_media.count}`
 
-                await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { caption: msg })
+            await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { caption: msg })
 
-            }).catch(async (err) => {
-                await message.sendMessage(errorMessage(Lang.NOT_FOUND + userName))
-            })
-        }
-        else if (userName.length >= 31) {
-
-            await message.sendMessage(infoMessage(Lang.LOADING))
-
-            await axios.get(`https://docs-jojo.herokuapp.com/api/insta?url=${userName}`).then(async (response) => {
-
-                const {is_video, url } = response.data.resource
-
-                const profileBuffer = await axios.get(url[0], { responseType: 'arraybuffer' })
-
-                if (`${is_video}` === "true") {
-                    await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, { caption: 'Made by WhatsAsena' })
-                }
-                else if (`${is_video}` === "false") {
-                    await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { caption: 'Made by WhatsAsena' })
-                }
-            }).catch(async (err) => {
-                await message.sendMessage(errorMessage(Lang.NOT_FOUND + userName))
-            })
-        }
-    });
-}
-if (cn.WORKTYPE == 'public') {
-
-    Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: Lang.DESC }, async (message, match) => {
-
-        const userName = match[1]
-
-        if (userName === '') return await message.sendMessage(errorMessage(Lang.NEED_WORD))
-        if (userName.length <= 30) {
-
-            await message.sendMessage(infoMessage(Lang.LOADING))
-
-            await axios.get(`https://docs-jojo.herokuapp.com/api/stalk?username=${userName}`).then(async (response) => {
-
-                const {biography, username, edge_follow, edge_followed_by, category_name, is_verified, is_private, edge_owner_to_timeline_media, profile_pic_url_hd, full_name, is_business_account } = response.data.result.graphql.user
-
-                const profileBuffer = await axios.get(profile_pic_url_hd, { responseType: 'arraybuffer' })
-
-                const msg = `*${Lang.NAME}*: ${full_name} \n*${Lang.USERNAME}*: ${username} \n*${Lang.BIO}*: ${biography} \n*${Lang.FOLLOWERS}*: ${edge_followed_by.count} \n*${Lang.FOLLOWS}*: ${edge_follow.count} \n*${Lang.ACCOUNT}*: ${is_private ? Lang.HIDDEN : Lang.PUBLIC} \n*Hesap Türü:* ${is_verified ? vf : novf} \n*İşletme Hesabı mı?:* ${is_business_account ? bs : nobs} \n*Kategori:* ${category_name} \n*Post Sayısı:* ${edge_owner_to_timeline_media.count}`
-
-                await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { caption: msg })
-
-            }).catch(async (err) => {
-                await message.sendMessage(errorMessage(Lang.NOT_FOUND + userName))
-            })
-        }
-        else if (userName.length >= 31) {
-
-            await message.sendMessage(infoMessage(Lang.LOADING))
-
-            await axios.get(`https://docs-jojo.herokuapp.com/api/insta?url=${userName}`).then(async (response) => {
-
-                const {is_video, url } = response.data.resource
-
-                const profileBuffer = await axios.get(url[0], { responseType: 'arraybuffer' })
-
-                if (`${is_video}` === "true") {
-                    await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, { caption: 'Made by WhatsAsena' })
-                }
-                else if (`${is_video}` === "false") {
-                    await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, { caption: 'Made by WhatsAsena' })
-                }
-            }).catch(async (err) => {
-                await message.sendMessage(errorMessage(Lang.NOT_FOUND + userName))
-            })
-        }
+        }).catch(async (err) => {
+            await message.sendMessage(errorMessage(Lang.NOT_FOUND + userName))
+        })
     });
 }
