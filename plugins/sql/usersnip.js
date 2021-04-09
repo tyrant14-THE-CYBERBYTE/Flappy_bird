a005eabd5287e33231883c5e136ceba609e3b2e4
Baran <57062330+Xenon67@users.noreply.github.com>
2021-04-09 16:25:17 +0300

    Create usersnip.js

---
+++ plugins/sql/usersnip.js
@@ -0,0 +1,42 @@
+/* Codded by @phaticusthiccy
+Telegram: t.me/phaticusthiccy
+Instagram: www.instagram.com/kyrie.baran
+*/
+
+const config = require('../../config');
+const { DataTypes } = require('sequelize');
+
+const SnipDB = config.DATABASE.define('snip', {
+    snip: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    }
+});
+
+
+async function getSnip() {
+    const Snip = await SnipDB.findAll()
+
+    return Snip
+}
+
+async function saveSnip(snip) {
+    return await SnipDB.create({ snip });
+}
+
+async function deleteSnip() {
+    var Msg = await SnipDB.findAll()
+
+    if (Msg.length < 1) {
+        return false;
+    } else {
+        return await Msg[0].destroy();
+    }
+}
+
+module.exports = {
+    SnipDB: SnipDB,
+    getSnip: getSnip,
+    saveSnip: saveSnip,
+    deleteSnip: deleteSnip
+};
