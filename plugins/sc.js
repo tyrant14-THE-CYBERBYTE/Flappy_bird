62381c3dcd03bf55592c891d403b32dfc0c09c81
Baran <57062330+Xenon67@users.noreply.github.com>
2021-04-11 03:51:25 +0300

    Update sc.js

--- plugins/sc.js
+++ plugins/sc.js
@@ -15,7 +15,8 @@ Asena.addCommand({ pattern: 'tiktok ?(.*)', fromMe: true, desc: ttom}, (async (m
 
     const options = {
         noWaterMark: true,
-        hdVideo: true
+        hdVideo: true,
+        proxy: '42.119.139.254:8080'
     }
 
     if (match[1].length < 20) {
@@ -24,22 +25,18 @@ Asena.addCommand({ pattern: 'tiktok ?(.*)', fromMe: true, desc: ttom}, (async (m
             usf,
             MessageType.text
         );
-        try {
-            const info = await TikTokScraper.getUserProfileInfo(`${match[1]}`, options)
 
-            const buff = await axios.get(`${info.user.avatarLarger}`, { responseType: 'arraybuffer' })
+        const info = await TikTokScraper.getUserProfileInfo(`${match[1]}`, options)
 
-            const capt = `*Kullanıcı Adı:* ${info.user.uniqueId} \n*Hesap İsmi:* ${info.user.nickname} \n*Takipçi Sayısı:* ${info.stats.followerCount} \n*Takip Edilen:* ${info.stats.followingCount} \n*Toplam Beğeni:* ${info.stats.heartCount} \n*Toplam Video Sayısı:* ${info.stats.videoCount} \n*Biyografi:* ${info.user.signature} \n*Onaylanmış Hesap mı?:* ${info.user.verified ? evt : hyr} \n*Hesap Türü:* ${user.privateAccount ? ghz : nghs}`    
+        const buff = await axios.get(`${info.user.avatarLarger}`, { responseType: 'arraybuffer' })
 
-            return await message.sendMessage(
-                Buffer.from(buff.data),
-                MessageType.image,
-                { caption: capt }
-            )
-        }
-        catch (error) {
-            console.log(error)
-        }
+        const capt = `*Kullanıcı Adı:* ${info.user.uniqueId} \n*Hesap İsmi:* ${info.user.nickname} \n*Takipçi Sayısı:* ${info.stats.followerCount} \n*Takip Edilen:* ${info.stats.followingCount} \n*Toplam Beğeni:* ${info.stats.heartCount} \n*Toplam Video Sayısı:* ${info.stats.videoCount} \n*Biyografi:* ${info.user.signature} \n*Onaylanmış Hesap mı?:* ${info.user.verified ? evt : hyr} \n*Hesap Türü:* ${user.privateAccount ? ghz : nghs}`    
+
+        return await message.sendMessage(
+            Buffer.from(buff.data),
+            MessageType.image,
+            { caption: capt }
+        )  
     }
     else {
         await message.client.sendMessage(
@@ -47,21 +44,16 @@ Asena.addCommand({ pattern: 'tiktok ?(.*)', fromMe: true, desc: ttom}, (async (m
             vsf,
             MessageType.text
         )
-        try {
-            const vid = await TikTokScraper.getVideoMeta(`${match[1]}`, options)
+        const vid = await TikTokScraper.getVideoMeta(`${match[1]}`, options)
 
-            const buffv = await axios.get(`${vid.collector.videoUrlNoWaterMark}`, { responseType: 'arraybuffer' })
+        const buffv = await axios.get(`${vid.collector.videoUrlNoWaterMark}`, { responseType: 'arraybuffer' })
 
-            const captv = `*Kullanıcı Adı:* ${vid.collector.authorMeta} \n*Açıklama:* ${vid.collector.text} \n*Beğeni Sayısı:* ${vid.collector.diggCount} \n*Yorum Sayısı:* ${vid.collector.commentCount} \n*İzlenme Sayısı:* ${vid.collector.playCount} \n*Paylaşım Sayısı:* ${vid.collector.shareCount} \n*Video Uzunluğu:* ${vid.collector.videoMeta.duration} Saniye \n*Müzik İsmi:* ${vid.collector.musicMeta.musicName} \n*Müzik Sahibi:* ${vid.collector.musicMeta.musicAuthor}* \n*Kullanıcı Linki:* https://tiktok.com/@${vid.collector.authorMeta}`  
+        const captv = `*Kullanıcı Adı:* ${vid.collector.authorMeta} \n*Açıklama:* ${vid.collector.text} \n*Beğeni Sayısı:* ${vid.collector.diggCount} \n*Yorum Sayısı:* ${vid.collector.commentCount} \n*İzlenme Sayısı:* ${vid.collector.playCount} \n*Paylaşım Sayısı:* ${vid.collector.shareCount} \n*Video Uzunluğu:* ${vid.collector.videoMeta.duration} Saniye \n*Müzik İsmi:* ${vid.collector.musicMeta.musicName} \n*Müzik Sahibi:* ${vid.collector.musicMeta.musicAuthor}* \n*Kullanıcı Linki:* https://tiktok.com/@${vid.collector.authorMeta}`  
 
-            return await message.sendMessage(
-                Buffer.from(buffv.data),
-                MessageType.video,
-                { caption: captv }
-            )
-        }
-        catch (error) {
-            console.log(error)
-        }
+        return await message.sendMessage(
+            Buffer.from(buffv.data),
+            MessageType.video,
+            { caption: captv }
+        )
     }
 }));
