73f70a9caf510d3417554df5fb3e903dcdaedb24
Baran <57062330+Xenon67@users.noreply.github.com>
2021-04-20 13:36:29 +0300

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -42,7 +42,7 @@ const rp = "*Report!*"
 const ss = "Adminleri Etiketler"
 Asena.addCommand({pattern: 'tagadmin', fromMe: true, desc: ss}, (async (message, match) => {
 
-    const getGroupAdmins = (participants) async => {
+    const getGroupAdmins = async (participants) => {
         admins = []
         for (let i of participants) {
                 i.isAdmin ? admins.push(i.jid) : ''
