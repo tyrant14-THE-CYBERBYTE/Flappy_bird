07eb7a27e52b6207c16b01900e8c609e5d08d97c
devf3lix <83315704+devf3lix@users.noreply.github.com>
2021-04-29 22:26:03 +0300

    Create uyaridefteri.js

---
+++ plugins/uyaridefteri.js
@@ -0,0 +1,14 @@
+const Asena = require('../events');
+const{MessageType} = require('@adiwajshing/baileys')
+
+Asena.addCommand({pattern: 'uyaridefteri', fromMe: true}, (async (message, match) => {
+
+    await message.sendMessage('*+90(534)077-0299 Uyarı Sayısı: 1*');
+    await new Promise(r => setTimeout(r, 1000)));
+  
+    await message.sendMessage('*+90(543)661-9918 Uyarı Sayısı: 2*');
+    await new Promise(r => setTimeout(r, 1000)));
+  
+    await message.sendMessage('```Her Gece 00.00 da Güncellenir.```');
+  
+}));
