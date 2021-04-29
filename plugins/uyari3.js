ac219fe979f1df0effe5eadf706c3e13ad001a8b
devf3lix <83315704+devf3lix@users.noreply.github.com>
2021-04-29 22:10:51 +0300

    Update uyari3.js

--- plugins/uyari3.js
+++ plugins/uyari3.js
@@ -4,7 +4,7 @@ const{MessageType} = require('@adiwajshing/baileys')
 const tor = "Kullanıcıya Uyarı gönderir."
 const rep = "*Lütfen Son Uyarıyı Vereceğim Kullanıcının Mesajına Yanıt Verin!*"
 const rap = "=== ```3.Uyarı ve Ban Sebebi``` ===\n\n"
-Asena.addCommand({pattern: 'uyari3 ?(.*)', fromMe: true, desc: tor}, (async (message, match) => {
+Asena.addCommand({pattern: 'uyari3 ?(.*)', fromMe: true, deleteCommand: false, desc: tor}, (async (message, match) => {
     if (match[1] == '' && message.reply_message) {
         let grup = await message.client.groupMetadata(message.jid);
         var jids = [];
