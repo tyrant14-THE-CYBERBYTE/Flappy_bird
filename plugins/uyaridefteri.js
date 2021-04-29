80ea487703ddd5583ca9d1f723a082f0e5b3badc
devf3lix <83315704+devf3lix@users.noreply.github.com>
2021-04-29 22:38:28 +0300

    Update uyaridefteri.js

--- plugins/uyaridefteri.js
+++ plugins/uyaridefteri.js
@@ -4,10 +4,10 @@ const{MessageType} = require('@adiwajshing/baileys')
 Asena.addCommand({pattern: 'uyaridefteri', fromMe: true}, (async (message, match) => {
 
     await message.sendMessage('*+90(534)077-0299 Uyarı Sayısı: 1*');
-    await new Promise(r => setTimeout(r, 1000)));
+    await new Promise(r => setTimeout(r, 1000));
   
     await message.sendMessage('*+90(543)661-9918 Uyarı Sayısı: 2*');
-    await new Promise(r => setTimeout(r, 1000)));
+    await new Promise(r => setTimeout(r, 1000));
   
     await message.sendMessage('```Her Gece 00.00 da Güncellenir.```');
   
