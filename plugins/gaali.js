76cbd937ce9c64d27c037b676631ce4b603bf76f
Sam Pandey <69663175+sam-pandey@users.noreply.github.com>
2021-05-11 05:26:30 +0000

    Create gaali.js

---
+++ plugins/gaali.js
@@ -0,0 +1,35 @@
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Asena.addCommand({pattern: 'gaali', fromMe: true}, (async (message, match) => {
+
+    var r_text = new Array ();    
+r_text[0] = "*Abey Ja Na Randi*";
+r_text[1] = "*Abey Ja Na Madarchod*";
+r_text[2] = "*Too mach gey*";
+r_text[3] = "You gey gey gey gey gey gey gey gey";
+r_text[4] = "Chut mai talwar ghusauga bc chut fat jaegi aur usme se itna khoon niklega ki maza hi ajaega deklena sale sasti jhat";
+r_text[5] = "Loda lasun madrchod teri maa ki chut gasti ama ka chutia bacha teri maa ko chod chod k pagal kar dunga maa k lody kisi sastiii randii k bachy teri maa ki choot main teer maarun gandu harami";
+r_text[6] = "Maa ke bhosde se bahr aja fir baap se zuban da teri maa ki chut chod chod ke bhosdabnadu madarchod aur uske upar cement lagadu ki tere jesa gandu insaan kabhi bahr na a ske esi gandi chut mai se";
+r_text[7] = "Teri college jati baji ka road pey rape karongandu ki olaad haram ki nasal papa hun tera bhen pesh kar ab papa ko teri maa kkale kuss main kisi!";
+r_text[8] = "Maccharki tatte ke baal chutiye maa ke chut pe ghode ka Lund tere gand me jaltha hu koila Dale bhen ke lode MAA KI";
+r_text[9] = "Tere baap NE teko kya khake paida kiya Tha kesa chutiya he tu rand ke bacche teko shadi me khana khane na mile teko gand pe 4 thappad mare sab log aur blade se likhe I want anal madarchod bosdike";
+r_text[10] = "Maa ke lode bandarchod tere gand me chitiya Kate wo bhi bullet ants maadarchod samj nahi aaraha";
+r_text[11] = "Drugs tere gand Me dalunga ki tatti nahi nikle maadarchod kabhi teko Marne ka mouka mil gaya na tho bas I'll do my best to get that tatti outof you aur tere jaise chutio ko is duniya me jagaha bhi nahi.";
+r_text[12] = "*Sale tere gand ke balo pe tel laga ke jala du me teko*";
+r_text[13] = "Tere saat Johnny sins rape Kare aur jab wo teko anal de tab loda andar fas Jaye bkl tere jhaat pe waxing karunga me dhek lio fir jab tu chillayega na tab tere muh me Mai gai ka gobar dalunga";
+r_text[14] = "Tere lode pe madhu makkhi Katelode ke ando pe Road roller chale tu kab bathroom me muthne Jaye tho Tera loda ghir Jaye fir tere ando me se lizard ke bacche nikle teko kidnap Kare aur childporn banaye maa ke chuttad ke lode";
+r_text[15] = "Tujhetho gali ke kutte gand pe chut rakh ke katenge me bata raha hu";
+r_text[16] = "Madarchod Randi ke bacche Oye bosdike madarchod bhen ke lode tere gand me lohe ka danda garam karke dalu randwe";
+r_text[17] = "Uss raat mene mehnat se gaddha khoda Fir kyu paida hua tere jaisa bokachoda";
+r_text[18] = "Joe mama farted so hard that gave birth to a Retard like u";
+r_text[19] = "Bhosdike Madarchod Behen ke lawde Macchar ki jhat Gandu Tatto ke sawdagar Maa ki chut teri";
+r_text[20] = "*Gand main Sariya dal dunga Behenchod*";
+r_text[21] = "*Why are you Gey*";
+
+var i = Math.floor(22*Math.random())
+
+await message.sendMessage(r_text[i]);
+
+}));
