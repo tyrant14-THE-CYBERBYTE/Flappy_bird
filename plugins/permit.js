bca5bf21ee13bcd3915f51c1001161ceb144a1d9
Sam Pandey <69663175+sam-pandey@users.noreply.github.com>
2021-05-11 05:31:01 +0000

    Update permit.js

--- plugins/permit.js
+++ plugins/permit.js
@@ -3,15 +3,15 @@ const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const Axios = require('axios');
 const Config = require('../config');
 const pmpermit = require('/root/WhatsAsenaDuplicated/plugins/pmpermit');
-const ds = "PM Permit iznini açar."
-const dds = "PM Permit iznini kapatır."
+const ds = "Turns PM Permit permission."
+const dds = "Turns off the PM Permit permission."
 Asena.addCommand({pattern: 'approve$', desc: ds, onlyPm: true}, (async (message, match) => { 
     pmpermit.permitacton(message.jid.split("@")[0])
-    await message.client.sendMessage(message.jid,'PM İzni Verildi!', MessageType.text);
+    await message.client.sendMessage(message.jid,'Approved for PM!', MessageType.text);
 }));
 Asena.addCommand({pattern: 'disapprove$', desc: dds, onlyPm: true}, (async (message, match) => { 
     pmpermit.nopermitacton(message.jid.split("@")[0])
-    await message.client.sendMessage(message.jid,'PM İzni Kaldırıldı!', MessageType.text);
+    await message.client.sendMessage(message.jid,'Disapproved for pm!', MessageType.text);
 }));
 
 Asena.addCommand({on: 'text', fromMe: false, onlyPm: true, deleteCommand: false, dontAddCommandList: true }, (async (message, match) => {
